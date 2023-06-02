fc2dc0825d1af8981356f042a8e188114eb732f8
Dev00kushal <devkushal00@gmail.com>
2023-06-02 18:43:06 +0545

    Fix: Video displaying error

--- Chapter-1/img_and_vid_caputure.py
+++ Chapter-1/img_and_vid_caputure.py
@@ -3,17 +3,19 @@ import cv2 as cv
 #  We will be reading the image
 img = cv.imread("Chapter-1/Resources/Lenna.png")
 cv.imshow("Image_analysis",img)
-cv.waitKey(0)
 
 # This is for the video 
 frame_width = 600
 frame_height = 600
 cap = cv.VideoCapture("Chapter-1/Resources/dog_video.mp4") 
-
 while True:
-    sucess, image = cap.read()
-    image = cv.resize(image,(frame_width,frame_height))
-    cv.imshow("Dog_video_analysis", image)
+     sucess, image = cap.read()
+     image = cv.resize(image,(frame_width,frame_height))
+     cv.imshow("Video_analysis", image)
 
-    if cv.waitKey(0) and 0XFF == ("q"):
-        break
+     if cv.waitKey(1) & 0XFF == ord("q"):
+                break   
+
+cv.release()
+cv.destroyAllWindows()
+
